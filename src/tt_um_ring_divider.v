a615e6bdc6651d6354fc6fe257d9815e6c164dc7
SmellyJelly01 <jarnail01@gmail.com>
2024-08-28 20:50:30 -0700

    fixed error in top module

--- src/tt_um_ring_divider.v
+++ src/tt_um_ring_divider.v
@@ -17,29 +17,17 @@ module tt_um_ring_divider (
 );
 	assign uio_oe = 8'b0000_0000;
 	assign uio_out = 8'b0000_0000;
-	assign uo_out[7:1] = 7'b0000_000
+	assign uo_out[7:1] = 7'b0000_000;
 	
-	wire n1, n2, n3, n4, osc_out;
-	frequency_divider freq_div (
-		.clk_in(osc_out),
-		.clk_out2(),
-		.clk_out4(),
-		.clk_out8(),
-		.clk_out16(),
-		.clk_out32(),
-		.clk_out64(),
-		.clk_out128(uo_out[0])
-    	);
-    
-    	RING_OSCILLATOR ring_osc (
-		.en0(ena),
-		.en1(ena),
-		.in(osc_out),
-		.out(osc_out),
-		.n1(n1),
-		.n2(n2),
-		.n3(n3),
-		.n4(n4)
-    	);
+	RING_DIVIDER RING_DIV (
+		.enable(ena),
+        	.enable1(ena),
+        	.osc_out(),
+        	.final_out(uo_out[0]),
+        	.n1(),
+        	.n2(),
+        	.n3(),
+        	.n4()
+	);
     
 endmodule
